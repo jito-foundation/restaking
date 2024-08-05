9fb262d8068f14ef6b7291ffbe2bc8bec9a76b7b
aoikurokawa <ht.so.ik0102@gmail.com>
2024-08-05 21:22:46 +0900

    fix: modify scripts

--- run_tests.sh
+++ run_tests.sh
@@ -5,9 +5,11 @@ TOKEN_METADATA_SO="$(pwd)/target/sbf-solana-solana/release/token_metadata.so"
 
 if [ ! -f "$TOKEN_METADATA_SO" ]; then
 	git clone https://github.com/metaplex-foundation/mpl-token-metadata
-	cd mpl-token-metadata/programs/token-metadata/program/token-metadata
+	pushd mpl-token-metadata/programs/token-metadata/program/
 	cargo build-bpf
-	cp target/sbf-solana-solana/release/token_metadata.so $(pwd)/target/sbf-solana-solana/release/
+	popd
+	cp mpl-token-metadata/programs/token-metadata/target/sbf-solana-solana/release/token_metadata.so $(pwd)/target/sbf-solana-solana/release/
+	rm -rf mpl-token-metadata
 fi
 
 cargo-build-sbf && SBF_OUT_DIR=$(pwd)/target/sbf-solana-solana/release cargo nextest run --all-features
