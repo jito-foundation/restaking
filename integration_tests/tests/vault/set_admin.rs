913d53305fbcb710101e7af219d0306e11345c98
aoikurokawa <ht.so.ik0102@gmail.com>
2024-08-16 20:33:30 +0900

    test: add new test case

--- integration_tests/tests/vault/set_admin.rs
+++ integration_tests/tests/vault/set_admin.rs
@@ -2,21 +2,18 @@
 mod tests {
     use jito_vault_core::config::Config;
     use jito_vault_sdk::{error::VaultError, instruction::VaultAdminRole};
+    use solana_program::pubkey::Pubkey;
     use solana_sdk::signature::{Keypair, Signer};
 
     use crate::fixtures::{
         fixture::TestBuilder,
-        vault_client::{assert_vault_error, VaultRoot},
+        vault_client::{assert_vault_error, VaultProgramClient, VaultRoot},
     };
 
-    #[tokio::test]
-    async fn test_set_admin_with_bad_admin() {
+    async fn setup() -> (VaultProgramClient, Pubkey, Keypair) {
         let fixture = TestBuilder::new().await;
 
         let mut vault_program_client = fixture.vault_program_client();
-
-        let deposit_fee_bps = 99;
-        let withdrawal_fee_bps = 100;
 
         let (
             _config_admin,
@@ -25,9 +22,16 @@ mod tests {
                 vault_admin,
             },
         ) = vault_program_client
-            .setup_config_and_vault(deposit_fee_bps, withdrawal_fee_bps)
+            .setup_config_and_vault(99, 100)
             .await
             .unwrap();
+
+        (vault_program_client, vault_pubkey, vault_admin)
+    }
+
+    #[tokio::test]
+    async fn test_set_admin_with_bad_admin() {
+        let (mut vault_program_client, vault_pubkey, vault_admin) = setup().await;
 
         let config_pubkey = Config::find_program_address(&jito_vault_program::id()).0;
         let vault = vault_program_client.get_vault(&vault_pubkey).await.unwrap();
@@ -50,23 +54,27 @@ mod tests {
 
     #[tokio::test]
     async fn test_set_admin() {
-        let fixture = TestBuilder::new().await;
+        let (mut vault_program_client, vault_pubkey, vault_admin) = setup().await;
 
-        let mut vault_program_client = fixture.vault_program_client();
+        let config_pubkey = Config::find_program_address(&jito_vault_program::id()).0;
+        let vault = vault_program_client.get_vault(&vault_pubkey).await.unwrap();
 
-        let deposit_fee_bps = 99;
-        let withdrawal_fee_bps = 100;
+        assert_eq!(vault.admin, vault_admin.pubkey());
 
-        let (
-            _config_admin,
-            VaultRoot {
-                vault_pubkey,
-                vault_admin,
-            },
-        ) = vault_program_client
-            .setup_config_and_vault(deposit_fee_bps, withdrawal_fee_bps)
+        let new_admin = Keypair::new();
+        vault_program_client
+            .set_admin(&config_pubkey, &vault_pubkey, &vault_admin, &new_admin)
             .await
             .unwrap();
+
+        let vault = vault_program_client.get_vault(&vault_pubkey).await.unwrap();
+
+        assert_eq!(vault.admin, new_admin.pubkey());
+    }
+
+    #[tokio::test]
+    async fn test_update_secondary_admin() {
+        let (mut vault_program_client, vault_pubkey, vault_admin) = setup().await;
 
         let config_pubkey = Config::find_program_address(&jito_vault_program::id()).0;
         let vault = vault_program_client.get_vault(&vault_pubkey).await.unwrap();
