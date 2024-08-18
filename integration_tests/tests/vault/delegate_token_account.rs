2d3527bdb0c4c947703a34c915e7e2da98befe0c
aoikurokawa <ht.so.ik0102@gmail.com>
2024-08-18 09:40:13 +0900

    test: supported_token does not allow

--- integration_tests/tests/vault/delegate_token_account.rs
+++ integration_tests/tests/vault/delegate_token_account.rs
@@ -7,10 +7,7 @@ mod tests {
 
     use crate::fixtures::{fixture::TestBuilder, vault_client::VaultRoot};
 
-    #[test_case(spl_token::id(); "token")]
-    #[test_case(spl_token_2022::id(); "token-2022")]
-    #[tokio::test]
-    async fn test_delegate_token_account_ok(token_program_id: Pubkey) {
+    async fn setup(token_program_id: &Pubkey) -> (TestBuilder, Pubkey, Keypair, Keypair, Keypair) {
         let mut fixture = TestBuilder::new().await;
 
         let mut vault_program_client = fixture.vault_program_client();
@@ -44,30 +41,6 @@ mod tests {
                 )
                 .await
                 .unwrap();
-            let ata = get_associated_token_address(&vault_pubkey, &random_mint.pubkey());
-            let token_account_acc = fixture.get_token_account(&ata).await.unwrap();
-
-            assert_eq!(token_account_acc.amount, 100_000);
-            assert_eq!(token_account_acc.delegate, COption::None);
-
-            // Delegate
-            let bob = Pubkey::new_unique();
-            vault_program_client
-                .delegate_token_account(
-                    &vault_pubkey,
-                    &vault_admin,
-                    &random_mint.pubkey(),
-                    &get_associated_token_address(&&vault_pubkey, &random_mint.pubkey()),
-                    &bob,
-                    &token_program_id,
-                    50_000,
-                )
-                .await
-                .unwrap();
-            let ata = get_associated_token_address(&vault_pubkey, &random_mint.pubkey());
-            let token_account_acc = fixture.get_token_account(&ata).await.unwrap();
-            assert_eq!(token_account_acc.delegate, COption::Some(bob));
-            assert_eq!(token_account_acc.delegated_amount, 50_000);
         } else {
             fixture
                 .create_token_account(
@@ -88,14 +61,46 @@ mod tests {
                 )
                 .await
                 .unwrap();
+        }
 
-            let vault_token_acc = fixture
-                .get_token_account(&vault_token_account.pubkey())
+        (
+            fixture,
+            vault_pubkey,
+            vault_admin,
+            random_mint,
+            vault_token_account,
+        )
+    }
+
+    #[test_case(spl_token::id(); "token")]
+    #[test_case(spl_token_2022::id(); "token-2022")]
+    #[tokio::test]
+    async fn test_delegate_token_account_ok(token_program_id: Pubkey) {
+        let (mut fixture, vault_pubkey, vault_admin, random_mint, vault_token_account) =
+            setup(&token_program_id).await;
+        let mut vault_program_client = fixture.vault_program_client();
+
+        if token_program_id.eq(&spl_token::id()) {
+            // Delegate
+            let bob = Pubkey::new_unique();
+            vault_program_client
+                .delegate_token_account(
+                    &vault_pubkey,
+                    &vault_admin,
+                    &random_mint.pubkey(),
+                    &get_associated_token_address(&&vault_pubkey, &random_mint.pubkey()),
+                    &bob,
+                    &token_program_id,
+                    50_000,
+                )
                 .await
                 .unwrap();
-            assert_eq!(vault_token_acc.amount, 100_000);
-            assert_eq!(vault_token_acc.delegate, COption::None);
+            let ata = get_associated_token_address(&vault_pubkey, &random_mint.pubkey());
+            let token_account_acc = fixture.get_token_account(&ata).await.unwrap();
 
+            assert_eq!(token_account_acc.delegate, COption::Some(bob));
+            assert_eq!(token_account_acc.delegated_amount, 50_000);
+        } else {
             let bob = Pubkey::new_unique();
             vault_program_client
                 .delegate_token_account(
@@ -114,12 +119,53 @@ mod tests {
                 .get_token_account(&vault_token_account.pubkey())
                 .await
                 .unwrap();
+
             assert_eq!(vault_token_acc.delegate, COption::Some(bob));
             assert_eq!(vault_token_acc.delegated_amount, 50_000);
         }
+    }
+
+    #[test_case(spl_token::id(); "token")]
+    #[test_case(spl_token_2022::id(); "token-2022")]
+    #[tokio::test]
+    async fn test_delegate_vault_supprted_token_account_err(token_program_id: Pubkey) {
+        let (fixture, vault_pubkey, vault_admin, random_mint, vault_token_account) =
+            setup(&token_program_id).await;
+        let mut vault_program_client = fixture.vault_program_client();
+
+        let vault = vault_program_client.get_vault(&vault_pubkey).await.unwrap();
 
         if token_program_id.eq(&spl_token::id()) {
+            // Delegate
+            let bob = Pubkey::new_unique();
+            let response = vault_program_client
+                .delegate_token_account(
+                    &vault_pubkey,
+                    &vault_admin,
+                    &&vault.supported_mint,
+                    &get_associated_token_address(&&vault_pubkey, &random_mint.pubkey()),
+                    &bob,
+                    &token_program_id,
+                    50_000,
+                )
+                .await;
+
+            assert!(response.is_err());
         } else {
+            let bob = Pubkey::new_unique();
+            let response = vault_program_client
+                .delegate_token_account(
+                    &vault_pubkey,
+                    &vault_admin,
+                    &&vault.supported_mint,
+                    &vault_token_account.pubkey(),
+                    &bob,
+                    &token_program_id,
+                    50_000,
+                )
+                .await;
+
+            assert!(response.is_err());
         }
     }
 }
