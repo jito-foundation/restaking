d2d0394cf43eb6f002de2ef001d2fbeab370882d
Lucas B <lucas@jito.wtf>
2024-08-18 20:57:17 -0500

    prevent vault balance during update

--- vault_program/src/update_vault_balance.rs
+++ vault_program/src/update_vault_balance.rs
@@ -1,6 +1,8 @@
 use jito_account_traits::AccountDeserialize;
 use jito_jsm_core::loader::load_associated_token_account;
 use jito_vault_core::{config::Config, vault::Vault};
+use solana_program::clock::Clock;
+use solana_program::sysvar::Sysvar;
 use solana_program::{
     account_info::AccountInfo, entrypoint::ProgramResult, program_error::ProgramError,
     program_pack::Pack, pubkey::Pubkey,
@@ -16,10 +18,14 @@ pub fn process_update_vault_balance(
     };
 
     Config::load(program_id, config, false)?;
+    let config_data = config.data.borrow();
+    let config = Config::try_from_slice(&config_data)?;
     Vault::load(program_id, vault_info, true)?;
     let mut vault_data = vault_info.data.borrow_mut();
     let vault = Vault::try_from_slice_unchecked_mut(&mut vault_data)?;
     load_associated_token_account(vault_token_account, vault_info.key, &vault.supported_mint)?;
+
+    vault.check_update_state_ok(Clock::get()?.slot, config.epoch_length)?;
 
     // TODO (LB): pay out fee account for any accrued fees to vault fee wallet
     vault.tokens_deposited = Account::unpack(&vault_token_account.data.borrow())?.amount;
