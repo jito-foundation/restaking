5d6a44965a799b8e0205cf35a4263a3c81fe5b8c
aoikurokawa <ht.so.ik0102@gmail.com>
2024-08-20 21:21:29 +0900

    fix: do not use is_update_needed, instead use check_update_state_ok

--- vault_core/src/vault.rs
+++ vault_core/src/vault.rs
@@ -300,22 +300,22 @@ impl Vault {
     // ------------------------------------------
 
     #[inline(always)]
-    pub fn is_update_needed(&self, slot: u64, epoch_length: u64) -> bool {
-        let last_updated_epoch = self
-            .last_full_state_update_slot
-            .checked_div(epoch_length)
-            .unwrap();
-        let current_epoch = slot.checked_div(epoch_length).unwrap();
-        last_updated_epoch < current_epoch
+    fn is_update_needed(&self, slot: u64, epoch_length: u64) -> Option<bool> {
+        let last_updated_epoch = self.last_full_state_update_slot.checked_div(epoch_length)?;
+        let current_epoch = slot.checked_div(epoch_length)?;
+        Some(last_updated_epoch < current_epoch)
     }
 
     #[inline(always)]
-    pub fn check_update_state_ok(&self, slot: u64, epoch_length: u64) -> Result<(), ProgramError> {
-        if self.is_update_needed(slot, epoch_length) {
-            msg!("Vault update is needed");
-            return Err(VaultError::VaultUpdateNeeded.into());
+    pub fn check_update_state_ok(&self, slot: u64, epoch_length: u64) -> Result<(), VaultError> {
+        match self.is_update_needed(slot, epoch_length) {
+            Some(true) => {
+                msg!("Vault update is needed");
+                Err(VaultError::VaultUpdateNeeded)
+            }
+            Some(false) => Ok(()),
+            None => unreachable!(),
         }
-        Ok(())
     }
 
     #[inline(always)]
