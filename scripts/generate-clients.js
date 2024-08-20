9a6f7500854a339270518b90e4c7131706cf13a1
Lucas B <lucas@jito.wtf>
2024-08-20 12:31:29 -0500

    generated code

--- scripts/generate-clients.js
+++ scripts/generate-clients.js
@@ -1,9 +1,21 @@
-import {renderJavaScriptVisitor, renderRustVisitor} from '@kinobi-so/renderers';
+const kinobi = require("kinobi");
+const anchorIdl = require("@kinobi-so/nodes-from-anchor");
+const path = require("path");
+const renderers = require('@kinobi-so/renderers');
 
-kinobi.accept(renderJavaScriptVisitor('../idl/testnet/', {
-    // Your JavaScript options here
-}));
+// Paths.
+const projectRoot = path.join(__dirname, "..");
 
-kinobi.accept(renderRustVisitor('../idl/testnet/', {
-    // Your Rust options here
+const idlDir = path.join(projectRoot, "idl", "testnet");
+
+const rustRestakingModule = path.join(__dirname, "..", "restaking_client");
+const rustRestakingGeneratedDir = path.join(rustRestakingModule, "src", "generated");
+
+const restaking_idl = require(path.join(idlDir, "jito_restaking_sdk.json"));
+const restaking_program = anchorIdl.rootNodeFromAnchor(restaking_idl);
+const restaking_kinobi = kinobi.createFromRoot(restaking_program)
+restaking_kinobi.accept(renderers.renderRustVisitor(path.join(rustRestakingGeneratedDir), {
+    formatCode: true,
+    crateFolder: rustRestakingModule,
+    deleteFolderBeforeRendering: true
 }));
