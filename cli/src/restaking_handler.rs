8861585e3d443ddd2c4e981a22ba54425eee174f
buffalu <85544055+buffalu@users.noreply.github.com>
2024-08-30 14:53:07 -0500

    Add support for custom mint decimals (#93)
    Feature: 
    - Adds support for custom mint decimals.
    
    Cleanup:
    - Rearranges some cli arguments into different module
    - Auto-generate CLI documents using clap-markdown

--- cli/src/restaking_handler.rs
+++ cli/src/restaking_handler.rs
@@ -1,7 +1,6 @@
 use std::str::FromStr;
 
 use anyhow::{anyhow, Result};
-use clap::Subcommand;
 use jito_bytemuck::{AccountDeserialize, Discriminator};
 use jito_restaking_client::instructions::{
     InitializeConfigBuilder, InitializeNcnBuilder, InitializeOperatorBuilder,
@@ -20,54 +19,10 @@ use solana_sdk::{
     transaction::Transaction,
 };
 
-use crate::cli_args::CliConfig;
-
-/// The CLI handler for the restaking program
-#[derive(Subcommand)]
-pub enum RestakingCommands {
-    /// Initialize, get, and set the config struct
-    Config {
-        #[command(subcommand)]
-        action: ConfigActions,
-    },
-    Ncn {
-        #[command(subcommand)]
-        action: NcnActions,
-    },
-    Operator {
-        #[command(subcommand)]
-        action: OperatorActions,
-    },
-}
-
-/// The actions that can be performed on the restaking config
-#[derive(Subcommand)]
-pub enum ConfigActions {
-    /// Initialize the config
-    Initialize,
-    /// Get the config
-    Get,
-}
-
-#[derive(Subcommand)]
-pub enum NcnActions {
-    /// Initialize NCN
-    Initialize,
-    /// Get NCN
-    Get { pubkey: String },
-    /// List all NCNs
-    List,
-}
-
-#[derive(Subcommand)]
-pub enum OperatorActions {
-    /// Initialize Operator
-    Initialize,
-    /// Get operator
-    Get { pubkey: String },
-    /// List all operators
-    List,
-}
+use crate::{
+    restaking::{ConfigActions, NcnActions, OperatorActions, RestakingCommands},
+    CliConfig,
+};
 
 pub struct RestakingCliHandler {
     cli_config: CliConfig,
