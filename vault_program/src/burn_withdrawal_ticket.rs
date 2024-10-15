a47b889d14f0e66ad3010bde19b048befc7c7a70
Evan Batsell <ebatsell@gmail.com>
2024-10-15 19:33:10 -0400

    remove debug statements

--- vault_program/src/burn_withdrawal_ticket.rs
+++ vault_program/src/burn_withdrawal_ticket.rs
@@ -33,22 +33,16 @@ pub fn process_burn_withdrawal_ticket(
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
-    msg!("loading config");
     Config::load(program_id, config, false)?;
     let config_data = config.data.borrow();
     let config = Config::try_from_slice_unchecked(&config_data)?;
-    msg!("loading vault");
     Vault::load(program_id, vault_info, true)?;
     let mut vault_data = vault_info.data.borrow_mut();
     let vault = Vault::try_from_slice_unchecked_mut(&mut vault_data)?;
-    msg!("loading vault token account");
     load_associated_token_account(vault_token_account, vault_info.key, &vault.supported_mint)?;
-    msg!("loading vrt mint");
     load_token_mint(vrt_mint)?;
     // staker
-    msg!("loading staker token account");
     load_associated_token_account(staker_token_account, staker.key, &vault.supported_mint)?;
-    msg!("loading vault staker withdrawal ticket");
     VaultStakerWithdrawalTicket::load(
         program_id,
         vault_staker_withdrawal_ticket_info,
@@ -59,23 +53,18 @@ pub fn process_burn_withdrawal_ticket(
     let vault_staker_withdrawal_ticket = VaultStakerWithdrawalTicket::try_from_slice_unchecked(
         &vault_staker_withdrawal_ticket_data,
     )?;
-    msg!("loading vault staker withdrawal ticket token account");
     load_associated_token_account(
         vault_staker_withdrawal_ticket_token_account,
         vault_staker_withdrawal_ticket_info.key,
         &vault.vrt_mint,
     )?;
-    msg!("loading vault fee token account");
     load_associated_token_account(vault_fee_token_account, &vault.fee_wallet, &vault.vrt_mint)?;
-    msg!("loading program fee token account");
     load_associated_token_account(
         program_fee_token_account,
         &config.program_fee_wallet,
         &vault.vrt_mint,
     )?;
-    msg!("loading token program");
     load_token_program(token_program)?;
-    msg!("loading system program");
     load_system_program(system_program)?;
 
     vault.check_mint_burn_admin(optional_accounts.first())?;
@@ -98,10 +87,8 @@ pub fn process_burn_withdrawal_ticket(
         vault_staker_withdrawal_ticket.vrt_amount(),
         min_amount_out,
     )?;
-    msg!("Decrementing VRT ready to claim amount");
     vault.decrement_vrt_ready_to_claim_amount(vault_staker_withdrawal_ticket.vrt_amount())?;
 
-    msg!("Finding program address for VaultStakerWithdrawalTicket");
     let (_, vault_staker_withdraw_bump, mut vault_staker_withdraw_seeds) =
         VaultStakerWithdrawalTicket::find_program_address(
             program_id,
@@ -116,7 +103,6 @@ pub fn process_burn_withdrawal_ticket(
     drop(vault_staker_withdrawal_ticket_data);
 
     // transfer fee to fee wallet
-    msg!("Transferring fee to fee wallet");
     invoke_signed(
         &transfer(
             &spl_token::id(),
@@ -134,7 +120,6 @@ pub fn process_burn_withdrawal_ticket(
         &[&seed_slices],
     )?;
     // Transfer program fee to program fee wallet
-    msg!("Transferring program fee to program fee wallet");
     invoke_signed(
         &transfer(
             &spl_token::id(),
@@ -153,7 +138,6 @@ pub fn process_burn_withdrawal_ticket(
     )?;
 
     // burn the VRT tokens
-    msg!("Burning VRT tokens");
     invoke_signed(
         &burn(
             &spl_token::id(),
@@ -172,7 +156,6 @@ pub fn process_burn_withdrawal_ticket(
     )?;
 
     // close token account
-    msg!("Closing token account");
     invoke_signed(
         &close_account(
             &spl_token::id(),
@@ -188,16 +171,13 @@ pub fn process_burn_withdrawal_ticket(
         ],
         &[&seed_slices],
     )?;
-    msg!("Closing program account");
     close_program_account(program_id, vault_staker_withdrawal_ticket_info, staker)?;
 
     // transfer the assets to the staker
-    msg!("Finding program address for Vault");
     let (_, vault_bump, mut vault_seeds) = Vault::find_program_address(program_id, &vault.base);
     vault_seeds.push(vec![vault_bump]);
     let seed_slices: Vec<&[u8]> = vault_seeds.iter().map(|seed| seed.as_slice()).collect();
     drop(vault_data); // avoid double borrow
-    msg!("Transferring assets to staker");
     invoke_signed(
         &transfer(
             &spl_token::id(),
