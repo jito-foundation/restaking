9f0bebedd212bca821e5af3da32f7c6abf7981b2
Evan B <ebatsell@gmail.com>
2024-10-24 22:35:03 -0400

    Update vault setup CLI command (#149)
    Needed new args

--- vault_sdk/src/instruction.rs
+++ vault_sdk/src/instruction.rs
@@ -9,7 +9,8 @@ pub enum VaultInstruction {
     #[account(0, writable, name = "config")]
     #[account(1, writable, signer, name = "admin")]
     #[account(2, name = "restaking_program")]
-    #[account(3, name = "system_program")]
+    #[account(3, name = "program_fee_wallet")]
+    #[account(4, name = "system_program")]
     InitializeConfig {
         program_fee_bps: u16,
     },
