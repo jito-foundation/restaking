aa109cb4599705a21f2c12fe7e988c25ac8cb75e
Christian Krueger <christian@jito.wtf>
2024-11-06 14:16:52 -0700

    fixed cooldown vault ncn

--- vault_program/src/cooldown_vault_ncn_ticket.rs
+++ vault_program/src/cooldown_vault_ncn_ticket.rs
@@ -19,26 +19,26 @@ pub fn process_cooldown_vault_ncn_ticket(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
 ) -> ProgramResult {
-    let [config, vault_info, ncn, vault_ncn_ticket, vault_ncn_admin] = accounts else {
+    let [config, vault, ncn, vault_ncn_ticket, vault_ncn_admin] = accounts else {
         return Err(ProgramError::NotEnoughAccountKeys);
     };
 
     Config::load(program_id, config, false)?;
     let config_data = config.data.borrow();
     let config = Config::try_from_slice_unchecked(&config_data)?;
-    Vault::load(program_id, vault_info, false)?;
-    let vault_data = vault_info.data.borrow();
-    let vault = Vault::try_from_slice_unchecked(&vault_data)?;
+    Vault::load(program_id, vault, false)?;
+    let vault_data = vault.data.borrow();
+    let vault_account = Vault::try_from_slice_unchecked(&vault_data)?;
     Ncn::load(&config.restaking_program, ncn, false)?;
-    VaultNcnTicket::load(program_id, vault_ncn_ticket, ncn, vault_info, true)?;
+    VaultNcnTicket::load(program_id, vault_ncn_ticket, vault, ncn, true)?;
     let mut vault_ncn_ticket_data = vault_ncn_ticket.data.borrow_mut();
     let vault_ncn_ticket =
         VaultNcnTicket::try_from_slice_unchecked_mut(&mut vault_ncn_ticket_data)?;
     load_signer(vault_ncn_admin, false)?;
 
-    vault.check_ncn_admin(vault_ncn_admin.key)?;
-    vault.check_update_state_ok(Clock::get()?.slot, config.epoch_length())?;
-    vault.check_is_paused()?;
+    vault_account.check_ncn_admin(vault_ncn_admin.key)?;
+    vault_account.check_update_state_ok(Clock::get()?.slot, config.epoch_length())?;
+    vault_account.check_is_paused()?;
 
     // The VaultNcnTicket must be active in order to cooldown the NCN
     if !vault_ncn_ticket
