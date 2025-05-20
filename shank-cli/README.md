f75820acaf427a39eba7ea8407a42f05a70abfbc
aoikurokawa <ht.so.ik0102@gmail.com>
2025-05-20 21:25:44 +0900

    feat: make jito-shank-cli be crate

---
+++ shank-cli/README.md
@@ -0,0 +1,93 @@
+# Jito Shank CLI
+
+A command-line interface tool for managing and generating IDL files for Solana programs using the shank-idl library.
+
+## Overview
+
+Jito Shank CLI simplifies the process of generating IDL files.
+It allows you to extract IDL definitions from multiple module paths and combine them into a single comprehensive IDL file.
+
+## Installation
+
+```bash
+cargo install jito-shank-cli
+```
+
+## Getting Started
+
+### Generate IDL
+
+### Command Structure
+
+The CLI supports the following commands and options:
+
+#### Global Options
+
+- `--program-env-path`: Path to the environment file containing program IDs
+- `--output-idl-path`: Directory where the generated IDL file will be saved
+
+#### Commands
+
+##### Generate
+
+Generates an IDL file.
+
+```bash
+shank-cli --program-env-path ./.env --output-idl-path ./idl generate --program-env-key MY_PROGRAM_ID --idl-name my_program --module-paths core program sdk
+```
+
+###### Options
+
+- `--program-env-key`: Key in the environment file that contains the program ID
+- `--idl-name`: Name for the generated IDL file
+- `--module-paths`: One or more paths to Rust modules containing shank annotations
+
+## Environment File Format
+
+The environment file should contain key-value pairs:
+
+```
+RESTAKING_PROGRAM_ID=RestkWeAVL8fRGgzhfeoqFhsqKRchg6aa1XrcH96z4Q
+VAULT_PROGRAM_ID=Vau1t6sLNxnzB7ZDsef8TLbPLfyZMYXH8WTNqUdm9g8
+```
+
+## Example
+
+Jito Restaking Program
+
+```bash
+cargo r -p jito-shank-cli -- \
+--program-env-path ./config/program.env \
+--output-idl-path ./idl \
+generate \
+--program-env-key "RESTAKING_PROGRAM_ID" \
+--idl-name jito_restaking \
+--module-paths "restaking_sdk" \
+--module-paths "restaking_core" \
+--module-paths "restaking_program" \
+--module-paths "bytemuck" \
+--module-paths "core"
+```
+
+This will:
+1. Read the program ID from the `PROGRAM_ID` key in `./config/program.env`
+2. Extract IDL definitions from the modules in `./restaking_sdk`, `./restaking_core`, `./restaking_program`, `./bytemuck` and `core`.
+3. Combine them into a single IDL
+4. Save the result as `./idl/jito_restaking.json`
+
+
+Jito Vault Program
+
+```bash
+cargo r -p jito-shank-cli -- \
+--program-env ./config/program.env \
+--output-idl-path ./idl \
+generate \
+--program-env-key "VAULT_PROGRAM_ID" \
+--idl-name jito_vault \
+--module-paths "vault_sdk" \
+--module-paths "vault_core" \
+--module-paths "vault_program" \
+--module-paths "bytemuck" \
+--module-paths "core"
+```
--- shank-cli/src/main.rs
+++ shank-cli/src/main.rs
@@ -1,118 +1,135 @@
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, path::PathBuf};
 
 use anyhow::{anyhow, Result};
+use clap::{Args, Parser, Subcommand};
 use env_logger::Env;
 use log::{debug, info};
 use shank_idl::{extract_idl, manifest::Manifest, ParseIdlOpts};
 
+#[derive(Parser)]
+#[command(author, version, about = "A CLI for managing shank", long_about = None)]
+struct Cli {
+    /// Path to the program_env file
+    #[arg(long)]
+    program_env_path: PathBuf,
+
+    /// Path to the idl
+    #[arg(long)]
+    output_idl_path: PathBuf,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate IDL
+    Generate(GenerateArgs),
+}
+
+#[derive(Args)]
+struct GenerateArgs {
+    /// Program id key in program.env
+    #[arg(long)]
+    program_id_key: String,
+
+    /// IDL names
+    #[arg(long)]
+    idl_name: String,
+
+    /// Module paths (core, program, sdk...)
+    #[arg(long)]
+    module_paths: Vec<String>,
+}
+
 struct IdlConfiguration {
     program_id: String,
-    name: &'static str,
-    paths: Vec<&'static str>,
+    name: String,
+    paths: Vec<String>,
 }
 
 fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let crate_root = std::env::current_dir()?;
 
-    let envs = envfile::EnvFile::new(crate_root.join("config").join("program.env"))?;
-    let restaking_program_id = envs
-        .get("RESTAKING_PROGRAM_ID")
-        .ok_or_else(|| anyhow!("RESTAKING_PROGRAM_ID not found"))?
-        .to_string();
-    let vault_program_id = envs
-        .get("VAULT_PROGRAM_ID")
-        .ok_or_else(|| anyhow!("VAULT_PROGRAM_ID not found"))?
-        .to_string();
+    let args: Cli = Cli::parse();
 
-    let idl_configs = vec![
-        IdlConfiguration {
-            program_id: restaking_program_id,
-            name: "jito_restaking",
-            paths: vec![
-                "restaking_sdk",
-                "restaking_core",
-                "restaking_program",
-                "bytemuck",
-                "core",
-            ],
-        },
-        IdlConfiguration {
-            program_id: vault_program_id,
-            name: "jito_vault",
-            paths: vec![
-                "vault_sdk",
-                "vault_core",
-                "vault_program",
-                "bytemuck",
-                "core",
-            ],
-        },
-    ];
-
-    let crate_root = std::env::current_dir().unwrap();
-    let out_dir = crate_root.join("idl");
-    for idl in idl_configs {
-        let mut idls = Vec::new();
-        for path in idl.paths {
-            let cargo_toml = crate_root.join(path).join("Cargo.toml");
-            if !cargo_toml.exists() {
-                return Err(anyhow!(
-                    "Did not find Cargo.toml at the path: {}",
-                    crate_root.display()
-                ));
-            }
-            let manifest = Manifest::from_path(&cargo_toml)?;
-            debug!("manifest: {:?}", manifest);
-            let lib_rel_path = manifest
-                .lib_rel_path()
-                .ok_or_else(|| anyhow!("Program needs to be a lib"))?;
-            debug!("lib_rel_path: {:?}", lib_rel_path);
-            let lib_full_path_str = crate_root.join(path).join(lib_rel_path);
-            let lib_full_path = lib_full_path_str
-                .to_str()
-                .ok_or_else(|| anyhow!("Invalid Path"))?;
-            debug!("lib_full_path: {:?}", lib_full_path);
-            // Extract IDL and convert to JSON
-            let opts = ParseIdlOpts {
-                program_address_override: Some(idl.program_id.to_string()),
-                ..ParseIdlOpts::default()
+    match args.command {
+        Commands::Generate(generate_args) => {
+            let envs = envfile::EnvFile::new(args.program_env_path)?;
+            let program_id = envs
+                .get(&generate_args.program_id_key)
+                .ok_or_else(|| anyhow!("{} not found", generate_args.program_id_key))?
+                .to_string();
+            let idl_config = IdlConfiguration {
+                program_id,
+                name: generate_args.idl_name,
+                paths: generate_args.module_paths,
             };
-            let idl = extract_idl(lib_full_path, opts)?
-                .ok_or_else(|| anyhow!("No IDL could be extracted"))?;
-            idls.push(idl);
-        }
 
-        let mut accumulator = idls.pop().unwrap();
-        for other_idls in idls {
-            accumulator.constants.extend(other_idls.constants);
-            accumulator.instructions.extend(other_idls.instructions);
-            accumulator.accounts.extend(other_idls.accounts);
-            accumulator.types.extend(other_idls.types);
-            if let Some(events) = other_idls.events {
-                if let Some(accumulator_events) = &mut accumulator.events {
-                    accumulator_events.extend(events);
-                } else {
-                    accumulator.events = Some(events);
+            let crate_root = std::env::current_dir().unwrap();
+            let mut idls = Vec::new();
+            for path in idl_config.paths.iter() {
+                let cargo_toml = crate_root.join(path).join("Cargo.toml");
+                if !cargo_toml.exists() {
+                    return Err(anyhow!(
+                        "Did not find Cargo.toml at the path: {}",
+                        crate_root.display()
+                    ));
+                }
+                let manifest = Manifest::from_path(&cargo_toml)?;
+                debug!("manifest: {:?}", manifest);
+                let lib_rel_path = manifest
+                    .lib_rel_path()
+                    .ok_or_else(|| anyhow!("Program needs to be a lib"))?;
+
+                debug!("lib_rel_path: {:?}", lib_rel_path);
+                let lib_full_path_str = crate_root.join(path).join(lib_rel_path);
+                let lib_full_path = lib_full_path_str
+                    .to_str()
+                    .ok_or_else(|| anyhow!("Invalid Path"))?;
+                debug!("lib_full_path: {:?}", lib_full_path);
+
+                // Extract IDL and convert to JSON
+                let opts = ParseIdlOpts {
+                    program_address_override: Some(idl_config.program_id.to_string()),
+                    ..ParseIdlOpts::default()
+                };
+                let idl = extract_idl(lib_full_path, opts)?
+                    .ok_or_else(|| anyhow!("No IDL could be extracted"))?;
+                idls.push(idl);
+            }
+
+            let mut accumulator = idls.pop().unwrap();
+            for other_idls in idls {
+                accumulator.constants.extend(other_idls.constants);
+                accumulator.instructions.extend(other_idls.instructions);
+                accumulator.accounts.extend(other_idls.accounts);
+                accumulator.types.extend(other_idls.types);
+                if let Some(events) = other_idls.events {
+                    if let Some(accumulator_events) = &mut accumulator.events {
+                        accumulator_events.extend(events);
+                    } else {
+                        accumulator.events = Some(events);
+                    }
+                }
+                if let Some(errors) = other_idls.errors {
+                    if let Some(accumulator_errors) = &mut accumulator.errors {
+                        accumulator_errors.extend(errors);
+                    } else {
+                        accumulator.errors = Some(errors);
+                    }
                 }
             }
-            if let Some(errors) = other_idls.errors {
-                if let Some(accumulator_errors) = &mut accumulator.errors {
-                    accumulator_errors.extend(errors);
-                } else {
-                    accumulator.errors = Some(errors);
-                }
-            }
+            accumulator.name = idl_config.name.to_string();
+
+            let idl_json = accumulator.try_into_json()?;
+            let mut idl_path = args.output_idl_path.join(idl_config.name);
+            idl_path.set_extension("json");
+
+            info!("Writing IDL to {:?}", idl_path);
+            let mut idl_json_file = File::create(idl_path)?;
+            idl_json_file.write_all(idl_json.as_bytes())?;
         }
-        accumulator.name = idl.name.to_string();
-
-        let idl_json = accumulator.try_into_json()?;
-        let mut idl_path = out_dir.join(idl.name);
-        idl_path.set_extension("json");
-
-        info!("Writing IDL to {:?}", idl_path);
-        let mut idl_json_file = File::create(idl_path)?;
-        idl_json_file.write_all(idl_json.as_bytes())?;
     }
 
     Ok(())
